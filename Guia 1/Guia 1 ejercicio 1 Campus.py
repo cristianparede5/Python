b7675c726f7d4ee79eb15b3720b7ea0cacdc9804
Cristian Paredes <99369108+cristianparede5@users.noreply.github.com>
2022-09-08 17:55:42 -0300

    Add files via upload

---
+++ Guia 1/Guia 1 ejercicio 1 Campus.py
@@ -0,0 +1,20 @@
+#1 - División con resto Plantear un algoritmo que permita informar, para dos valores a y b el resultado de la división a/b y el resto de esa división.
+
+#Declaración de Variables
+numeroA=float()
+numeroB=float()
+
+#Inicialización de Variables
+numeroA=0.0;
+numeroB=0.0;
+
+#Codigo
+#Números 'A' y 'B' a restar
+numeroA=float(input('Diga el número ''A'' a restar:'))
+numeroB=float(input('Diga en número ''B'' a restar:'))
+
+#Cuenta
+resta=numeroA-numeroB;
+
+#Muestra a
+print('la resta es de: ', resta)
